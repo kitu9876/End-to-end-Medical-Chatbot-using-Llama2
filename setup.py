b798c2fcb5eb3e444613cdd592c865c0a935d463
Kiran Bagade <kitu9876.kb@gmail.com>
2024-01-05 12:26:44 +0530

    add my all files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Kiran Bagade',
+    author_email= 'kitu9876.kb@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
